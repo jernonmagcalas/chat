a21ba6d9d85465de772ff556eb90b82a3f97b813
jernon magcalas <jernonmagcalas@gmail.com>
2017-02-09 02:36:51 +0800

    clean

--- src/server/console/commands.ts
+++
@@ -1,3 +0,0 @@
-import { CommandMapping } from 'chen/console';
-
-export const COMMANDS: CommandMapping = {};
--- src/server/http/routes.ts
+++
@@ -1,7 +0,0 @@
-import { Router } from 'chen/web';
-import { Config } from 'chen/core';
-
-export default function (router: Router, config: Config) {
-
-  router.route('GET', '/', 'IndexController@index');
-}
